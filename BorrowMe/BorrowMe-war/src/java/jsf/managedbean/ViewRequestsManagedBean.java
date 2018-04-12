f5ea8bcfb8b792bedae439ca2dac447e0acf2bd4
Fabian <fabianlim.2202@gmail.com>
2018-04-13 01:16:39 +0800

    made some progress with view request/etc

--- BorrowMe/BorrowMe-war/src/java/jsf/managedbean/ViewRequestsManagedBean.java
+++ BorrowMe/BorrowMe-war/src/java/jsf/managedbean/ViewRequestsManagedBean.java
@@ -2,6 +2,7 @@ package jsf.managedbean;
 
 import ejb.session.stateless.CustomerSessionBeanLocal;
 import entity.CustomerEntity;
+import entity.ListingEntity;
 import entity.RequestEntity;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -20,24 +21,31 @@ public class ViewRequestsManagedBean implements Serializable {
     @EJB(name = "CustomerSessionBeanLocal")
     private CustomerSessionBeanLocal customerSessionBeanLocal;
 
-    
-    
     private CustomerEntity customer;
     private List<RequestEntity> requestsMade;
-    
-    
+    private List<RequestEntity> filteredRequestsMade;
+    private List<ListingEntity> listings;
+    private RequestEntity selectedRequest;
+
     public ViewRequestsManagedBean() {
         customer = new CustomerEntity();
         requestsMade = new ArrayList<RequestEntity>();
-        
+        listings = new ArrayList<ListingEntity>();
+        filteredRequestsMade = new ArrayList<RequestEntity>();
+        selectedRequest = new RequestEntity();
     }
-    
+
     @PostConstruct
     public void postConstruct() {
         CustomerEntity c = (CustomerEntity) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("currentCustomerEntity");
         try {
-        c = customerSessionBeanLocal.retrieveCustomerByCustomerId(c.getCustomerId());
-        setRequestsMade(c.getRequestList());
+            c = customerSessionBeanLocal.retrieveCustomerByCustomerId(c.getCustomerId());
+            System.out.println("retrieved customer successfully from context; viewrequests managed bean");
+            setRequestsMade(c.getRequestList());
+            setListings(c.getListingList());
+            for (RequestEntity r : requestsMade) {
+                filteredRequestsMade.add(r);
+            }
         } catch (CustomerNotFoundException ex) {
             System.out.println("Customer not found in post construct of view requests managed bean");
         }
@@ -58,5 +66,29 @@ public class ViewRequestsManagedBean implements Serializable {
     public void setRequestsMade(List<RequestEntity> requestsMade) {
         this.requestsMade = requestsMade;
     }
-    
+
+    public List<RequestEntity> getFilteredRequestsMade() {
+        return filteredRequestsMade;
+    }
+
+    public void setFilteredRequestsMade(List<RequestEntity> filteredRequestsMade) {
+        this.filteredRequestsMade = filteredRequestsMade;
+    }
+
+    public List<ListingEntity> getListings() {
+        return listings;
+    }
+
+    public void setListings(List<ListingEntity> listings) {
+        this.listings = listings;
+    }
+
+    public RequestEntity getSelectedRequest() {
+        return selectedRequest;
+    }
+
+    public void setSelectedRequest(RequestEntity selectedRequest) {
+        this.selectedRequest = selectedRequest;
+    }
+
 }
